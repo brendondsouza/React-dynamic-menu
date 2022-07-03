173fc1bb7f7a2c0fe28a0ca023f7267681c12981
Brendon DSouza <brendonmufc@gmail.com>
2022-07-03 12:01:49 -0400

    initial commit

---
+++ build/precache-manifest.8535283faf4faa40045e657d530d7ff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbd7cf2f6502e58d834db1812a68e53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df6a1f684ee1ea398b3c",
+    "url": "/static/css/main.eab4bed5.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df6a1f684ee1ea398b3c",
+    "url": "/static/js/main.5fae262a.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
